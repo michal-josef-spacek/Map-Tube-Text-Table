41450b7ef2911066fbbca46fed6690465b7f4c33
Michal Špaček <tupinek@gmail.com>
2014-12-19 08:20:16 +0100

    Map::Tube::Text::Table::Utils: Add doc.

--- Table/Utils.pm
+++ Table/Utils.pm
@@ -85,3 +85,91 @@ sub _columns {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Map::Tube::Text::Table::Utils - Utilities for Map::Tube::Text::Table.
+
+=head1 SYNOPSIS
+
+ use Map::Tube::Text::Table::Utils qw(table);
+ my $table = table($title, $data_len_ar, $header_ar, $data_ar);
+
+=head1 SUBROUTINES
+
+=over 8
+
+=item C<table($title, $data_len_ar, $header_ar, $data_ar)>
+
+ Print table.
+ Returns text.
+
+=back
+
+=head1 EXAMPLE1
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Module.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Text::Table::Utils qw(table);
+
+ # Get table.
+ my $table = table('Title', [1, 2, 3], ['A', 'BB', 'CCC'], [
+         ['E', 'A', 'A'],
+         ['A', 'Ga', 'Acv'],
+ ]);
+
+ # Print table.
+ print encode_utf8($table);
+
+ # Output:
+ # ┌───────────┐
+ # │ Title     │
+ # ├──┬───┬────┤
+ # │ A│ BB│ CCC│
+ # ├──┼───┼────┤
+ # │ E│ A │ A  │
+ # │ A│ Ga│ Acv│
+ # └──┴───┴────┘
+
+=head1 DEPENDENCIES
+
+L<Exporter>,
+L<List::Util>,
+L<Readonly>,
+L<Text::UnicodeBox>,
+L<Text::UnicodeBox::Control>.
+
+=head1 SEE ALSO
+
+L<Map::Tube>,
+L<Map::Tube::Text::Table>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Map-Tube-Text-Table>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2014 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
